c1d4723017e3f732e09c5f590c112c5a6ee05b9b
Piotr Binkowski <pbinkowski@antmicro.com>
2020-02-18 16:02:33 +0100

    Add LiteUART sample

---
+++ samples/liteuart/sim_main.cpp
@@ -0,0 +1,84 @@
+//
+// Copyright (c) 2010-2020 Antmicro
+//
+//  This file is licensed under the MIT License.
+//  Full license text is available in 'LICENSE' file.
+//
+#include <verilated.h>
+#include "Vtop.h"
+#include <bitset>
+#include <stdio.h>
+#include <stdlib.h>
+#if VM_TRACE
+# include <verilated_fst_c.h>
+#endif
+// uart.h and axilite.h can be found in Renode's VerilatorPlugin
+#include "src/peripherals/uart.h"
+#include "src/buses/wishbone.h"
+
+#define UART_FREQ 100000000
+#define BAUDRATE 115200
+
+const int prescaler = UART_FREQ / (BAUDRATE * 8);
+UART *uart;
+Vtop *top;
+VerilatedFstC *tfp;
+vluint64_t main_time = 0;
+
+void eval() {
+#if VM_TRACE
+        main_time++;
+        tfp->dump(main_time);
+#endif
+    top->eval();
+    uart->eval();
+}
+
+void Init() {
+    Wishbone* bus = new Wishbone();
+
+    //=================================================
+    // Init bus signals
+    //=================================================
+    bus->wb_clk = &top->clk100;
+    bus->wb_rst = &top->rst0;
+    bus->wb_addr = (unsigned long *)&top->wb_adr;
+    bus->wb_rd_dat = (unsigned long *)&top->wb_dat_r;
+    bus->wb_wr_dat = (unsigned long *)&top->wb_dat_w;
+    bus->wb_we = &top->wb_we;
+    bus->wb_sel = &top->wb_sel;
+    bus->wb_stb = &top->wb_stb;
+    bus->wb_ack = &top->wb_ack;
+    bus->wb_cyc = &top->wb_cyc;
+
+    //=================================================
+    // Init eval function
+    //=================================================
+    bus->evaluateModel = &eval;
+
+    //=================================================
+    // Init peripheral
+    //=================================================
+    const int litex_rxtx_reg = 0x800;
+    uart = new UART(bus, &top->serial_tx, &top->serial_rx, prescaler, litex_rxtx_reg, &top->irq_uart0);
+}
+
+int main(int argc, char **argv, char **env) {
+    if(argc < 3) {
+        printf("Usage: %s {receiverPort} {senderPort}\n", argv[0]);
+        exit(-1);
+    }
+
+    Verilated::commandArgs(argc, argv);
+    top = new Vtop;
+#if VM_TRACE
+    Verilated::traceEverOn(true);
+    tfp = new VerilatedFstC;
+    top->trace(tfp, 99);
+    tfp->open("simx.fst");
+#endif
+    Init();
+    uart->simulate(atoi(argv[1]), atoi(argv[2]));
+    top->final();
+    exit(0);
+}
