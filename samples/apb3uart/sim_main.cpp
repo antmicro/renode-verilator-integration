f9b4139a5f8d22f55da937f89c1f9ec206fdf9db
Andrzej Drabarek <adrabarek@antmicro.com>
2024-11-26 16:37:10 +0100

    [#69274] Add explicit instantiation of VerilatedContext in apb3uart sample

--- samples/apb3uart/sim_main.cpp
+++ samples/apb3uart/sim_main.cpp
@@ -20,21 +20,29 @@
 #define UART_FREQ 50000000
 
 const int prescaler = UART_FREQ / (BAUDRATE * 8);
-UART *uart;
-VApb3UartCtrl *uart_ctrl = new VApb3UartCtrl;
+UART *uart = nullptr;
+VApb3UartCtrl *uart_ctrl = nullptr;
 VerilatedFstC *tfp;
-vluint64_t main_time = 0;
 
 void eval() {
 #if VM_TRACE
-        main_time++;
-        tfp->dump(main_time);
+    static vluint64_t main_time = 0;
+    main_time++;
+    tfp->dump(main_time);
 #endif
     uart_ctrl->eval();
     uart->eval();
 }
 
 RenodeAgent *Init() {
+    // These need static lifetime because we assign them to global pointers.
+    // By having the initialization here we guarantee that any static initialization
+    // from other compilation units (e. g. Verilator) is already done.
+    static VerilatedContext context;
+    static VApb3UartCtrl uart_ctrl_obj(&context);
+
+    uart_ctrl = &uart_ctrl_obj;
+
     APB3* bus = new APB3();
 
     //=================================================
@@ -59,7 +67,8 @@ RenodeAgent *Init() {
     // Init peripheral
     //=================================================
     const int murax_rxtx_reg = 0x0;
-    uart = new UART(bus, &uart_ctrl->io_uart_txd, &uart_ctrl->io_uart_rxd, prescaler, murax_rxtx_reg, &uart_ctrl->io_interrupt);
+    static UART uart_obj = UART(bus, &uart_ctrl->io_uart_txd, &uart_ctrl->io_uart_rxd, prescaler, murax_rxtx_reg, &uart_ctrl->io_interrupt);
+    uart = &uart_obj;
     return uart;
 }
 
@@ -70,6 +79,7 @@ int main(int argc, char **argv, char **env) {
     }
     const char *address = argc < 4 ? "127.0.0.1" : argv[3];
 
+    Init();
     Verilated::commandArgs(argc, argv);
 #if VM_TRACE
     Verilated::traceEverOn(true);
@@ -77,8 +87,11 @@ int main(int argc, char **argv, char **env) {
     uart_ctrl->trace(tfp, 99);
     tfp->open("simx.fst");
 #endif
-    Init();
     uart->simulate(atoi(argv[1]), atoi(argv[2]), address);
     uart_ctrl->final();
+
+#if VM_TRACE
+    delete tfp;
+#endif
     exit(0);
 }
